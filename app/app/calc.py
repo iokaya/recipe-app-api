323b66f97cb1b43aac24afc60580c12612a1a43f
iokaya <ismailonurkaya@gmail.com>
2022-06-01 09:43:10 +0300

    db conf added to docker-compose

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+calculator functions
+"""
+
+def add(x, y):
+    """Add x & y and return the results"""
+    return x + y
+
+def subtract(x, y):
+    """subtract x from y and return the result"""
+    return y - x
